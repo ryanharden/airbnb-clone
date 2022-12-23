b71ddf8ded84482dec68a4e7a4ce8236b4c03fe0
Ryan Harden <ryanharden20@gmail.com>
2022-12-22 16:14:02 -0800

    finished get-all-current-user-bookings

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,6 +6,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require("./spots");
 const reviewsRouter = require("./reviews");
+const bookingsRouter = require("./bookings");
 
 // GET /api/set-token-cookie
 router.get('/set-token-cookie', async (_req, res) => {
@@ -29,6 +30,8 @@ router.use('/users', usersRouter);
 router.use("/spots", spotsRouter);
 
 router.use("/reviews", reviewsRouter);
+
+router.use("/bookings", bookingsRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
