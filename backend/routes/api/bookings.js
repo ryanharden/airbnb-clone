53eba82aacdf4ae40dd91feb5ebf0fbf8485ed9e
Ryan Harden <ryanharden20@gmail.com>
2022-12-23 08:28:03 -0800

    finished edit-a-booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -58,4 +58,72 @@ router.get("/current", requireAuth, async (req, res) => {
 
 });
 
+// Edit a Booking
+
+router.put("/:bookingId", requireAuth, async (req, res) => {
+    const { startDate, endDate } = req.body;
+    const bookingId = +req.params.bookingId;
+    const currentUserId = req.user.id;
+    const startDateInt = new Date(startDate).getTime();
+    const endDateInt = new Date(endDate).getTime();
+
+    const oldBooking = await Booking.findOne({
+        where: {
+            id: bookingId,
+            userId: currentUserId
+        }
+    });
+
+    if (!oldBooking) {
+        res.status(404);
+        res.json({
+            "message": "Booking couldn't be found",
+            "statusCode": 404
+        })
+    }
+
+    if (startDateInt >= endDateInt) {
+        res.status(400);
+        return res.json({
+            "message": "Validation error",
+            "statusCode": 400,
+            "errors": {
+                "endDate": "endDate cannot be on or before startDate"
+            }
+        })
+    }
+
+    const bookingStartDate = new Date(oldBooking.startDate).getTime();
+    const bookingEndDate = new Date(oldBooking.endDate).getTime();
+
+    if (startDateInt <= Date.now() || bookingStartDate <= Date.now()) {
+        res.status(403);
+        return res.json({
+            "message": "Past bookings can't be modified",
+            "statusCode": 403
+        })
+    };
+
+    if ((startDateInt >= bookingStartDate && startDateInt < bookingEndDate) ||
+        (endDateInt > bookingStartDate && endDateInt <= bookingEndDate)) {
+        res.status(403);
+        return res.json({
+            "message": "Sorry, this spot is already booked for the specified dates",
+            "statusCode": 403,
+            "errors": {
+                "startDate": "Start date conflicts with an existing booking",
+                "endDate": "End date conflicts with an existing booking"
+            }
+        })
+    }
+
+   await oldBooking.update({
+        startDate,
+        endDate
+    });
+    res.json(oldBooking)
+});
+
+
+
 module.exports = router;
