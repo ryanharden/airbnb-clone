91407c9b882de62c06bcc7835035e153f875ffde
Ryan Harden <ryanharden20@gmail.com>
2023-01-13 11:20:10 -0800

    sign up thunk added

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -38,6 +38,24 @@ export const restoreUser = () => async dispatch => {
     return response;
 };
 
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+
+    const res = await csrfFetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            firstName,
+            lastName,
+            email,
+            password
+        }),
+    });
+    const data = await res.json();
+    dispatch(setUser(data.user));
+    return res;
+}
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
