a8a3cbdeea78c8cf0222bcc8a1d0ff43585a2c49
Ryan Harden <ryanharden20@gmail.com>
2023-01-17 13:00:33 -0800

    update

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -45,9 +45,9 @@ router.post(
       return res.json({
         "message": "User already exists",
         "statusCode": 403,
-        "errors": {
-          "email": "User with that email already exists"
-        }
+        "errors": [
+          "User with that email already exists"
+        ]
       })
     }
 
@@ -56,9 +56,9 @@ router.post(
       return res.json({
         "message": "User already exists",
         "statusCode": 403,
-        "errors": {
-          "username": "User with that username already exists"
-        }
+        "errors": [
+          "User with that username already exists"
+        ]
       })
     }
 
