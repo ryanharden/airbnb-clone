6bb4e7c5d55d517a43d8803e7ff7deb64fa54c25
Ryan Harden <ryanharden20@gmail.com>
2023-01-19 22:02:34 -0800

    added review-date

--- frontend/src/components/SpotReviewsIndexItem/SpotReviewsIndexItem.js
+++ frontend/src/components/SpotReviewsIndexItem/SpotReviewsIndexItem.js
@@ -4,15 +4,22 @@ import OpenModalButton from "../OpenModalButton";
 import profPic from "../../assets/prof-pic.jpeg";
 
 const SpotReviewsIndexItem = ({ review, spot }) => {
+    const date = new Date(review.createdAt);
+    const dateString = date.toLocaleDateString("en-US", { month: "short", year: "numeric" });
     return (
         <>
             <div className="spot-review-item-container">
                 <div className="spot-review-item-header">
                     <div className="creator-pic">
-                    <img className="prof-pic" src={profPic} alt="No picture" />
+                        <img className="prof-pic" src={profPic} alt="No picture" />
                     </div>
-                    <div className="creator-name">
-                        {review.User.firstName}
+                    <div className="name-date">
+                        <div className="creator-name">
+                            {review.User.firstName}
+                        </div>
+                        <div className="created-review-date">
+                            {dateString}
+                        </div>
                     </div>
                 </div>
                 <div className="review-text">
