fa66e3668d3d7188b293a8af22c5eda3c1803709
Ryan Harden <ryanharden20@gmail.com>
2023-01-21 22:14:13 -0800

    more css

--- frontend/src/components/CreateSpot/CreateSpot.js
+++ frontend/src/components/CreateSpot/CreateSpot.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useModal } from "../../context/Modal";
 import { createSpotThunk } from '../../store/spots';
 import { useHistory } from 'react-router-dom';
@@ -9,6 +9,7 @@ const CreateSpot = () => {
     const dispatch = useDispatch();
     const history = useHistory();
 
+    const sessionUser = useSelector((state) => state.session.user);
     const [name, setName] = useState('');
     const [address, setAddress] = useState('');
     const [city, setCity] = useState('');
@@ -40,14 +41,14 @@ const CreateSpot = () => {
         };
 
         dispatch(createSpotThunk(newSpotData, previewImage))
-        .then((res) => history.push(`/spots/${res.id}`))
-        // .then((res) => setNewSpot(res))
-        // .then((res) => console.log(res))
-        .then(closeModal())
-        .catch(async (res) => {
-            const data = await res.json();
-            if (data && data.errors) setErrors(data.errors)
-        });
+            .then((res) => history.push(`/spots/${res.id}`))
+            // .then((res) => setNewSpot(res))
+            // .then((res) => console.log(res))
+            .then(closeModal())
+            .catch(async (res) => {
+                const data = await res.json();
+                if (data && data.errors) setErrors(data.errors)
+            });
     };
 
     // useEffect(() => {
@@ -59,74 +60,79 @@ const CreateSpot = () => {
 
     return (
         <>
-            <div className='spot-form-header'>
-                <h1>List your nest</h1>
-            </div>
-            <div className='spot-form-container'>
-                <form className='spot-form' onSubmit={handleSubmit}>
-                    <ul className='errors-spot'>
-                        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-                    </ul>
-                    <input className='spot-form-input-name'
-                        type="text"
-                        placeholder="Name"
-                        required
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                    <input className='spot-form-input'
-                        type="text"
-                        placeholder="Address"
-                        required
-                        value={address}
-                        onChange={(e) => setAddress(e.target.value)}
-                    />
-                    <input className='spot-form-input'
-                        type="text"
-                        placeholder="City"
-                        required
-                        value={city}
-                        onChange={(e) => setCity(e.target.value)}
-                    />
-                    <input className='spot-form-input'
-                        type="text"
-                        placeholder="State"
-                        required
-                        value={state}
-                        onChange={(e) => setState(e.target.value)}
-                    />
-                    <input className='spot-form-input'
-                        type="text"
-                        placeholder="Country"
-                        required
-                        value={country}
-                        onChange={(e) => setCountry(e.target.value)}
-                    />
-                    <textarea className='spot-form-input'
-                        placeholder="Description"
-                        required
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                    />
-                    <input className='spot-form-input'
-                        type="number"
-                        placeholder="Price"
-                        required
-                        min="1"
-                        max="2000"
-                        value={price}
-                        onChange={(e) => setPrice(e.target.value)}
-                    />
-                    <input className='spot-form-input-url'
-                        type="url"
-                        placeholder="Preview Image Link"
-                        required
-                        value={previewImage}
-                        onChange={(e) => setPreviewImage(e.target.value)}
-                    />
-                    <button className='spot-form-button' type="submit">Create New Spot</button>
-                </form>
-            </div>
+            {sessionUser ?
+                <>
+                    <div className='spot-form-header'>
+                        <h1>List your nest</h1>
+                    </div>
+                    <div className='spot-form-container'>
+                        <form className='spot-form' onSubmit={handleSubmit}>
+                            <ul className='errors-spot'>
+                                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                            </ul>
+                            <input className='spot-form-input-name'
+                                type="text"
+                                placeholder="Name"
+                                required
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                            <input className='spot-form-input'
+                                type="text"
+                                placeholder="Address"
+                                required
+                                value={address}
+                                onChange={(e) => setAddress(e.target.value)}
+                            />
+                            <input className='spot-form-input'
+                                type="text"
+                                placeholder="City"
+                                required
+                                value={city}
+                                onChange={(e) => setCity(e.target.value)}
+                            />
+                            <input className='spot-form-input'
+                                type="text"
+                                placeholder="State"
+                                required
+                                value={state}
+                                onChange={(e) => setState(e.target.value)}
+                            />
+                            <input className='spot-form-input'
+                                type="text"
+                                placeholder="Country"
+                                required
+                                value={country}
+                                onChange={(e) => setCountry(e.target.value)}
+                            />
+                            <textarea className='spot-form-input'
+                                placeholder="Description"
+                                required
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                            />
+                            <input className='spot-form-input'
+                                type="number"
+                                placeholder="Price"
+                                required
+                                min="1"
+                                max="2000"
+                                value={price}
+                                onChange={(e) => setPrice(e.target.value)}
+                            />
+                            <input className='spot-form-input-url'
+                                type="url"
+                                placeholder="Preview Image Link"
+                                required
+                                value={previewImage}
+                                onChange={(e) => setPreviewImage(e.target.value)}
+                            />
+                            <button className='spot-form-button' type="submit">Create New Spot</button>
+                        </form>
+                    </div>
+                </>
+                : <div className='not-logged-in'>Sorry bucko, Please be logged in to do this.</div>
+            }
         </>
     )
 }
