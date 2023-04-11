b5d8a7380a7709bda403fbc422ab6e60d213f791
Ryan Harden <ryanharden20@gmail.com>
2023-04-10 19:13:50 -0700

    search bar css

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -65,32 +65,34 @@ const SearchBar = () => {
     // }
 
     return (
-        <>
-            <Wrapper apiKey={process.env.REACT_APP_MAPS_API_KEY} libraries={["places"]}>
-                <PlacesAutocomplete
-                    value={address}
-                    onChange={handleChange}
-                    onSelect={handleSelect}
-                >
-                    {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-                        <div>
-                            <input {...getInputProps({ placeholder: "Search City" })} />
-
-                            <div>
-                                {loading && <div>Loading...</div>}
-                                {suggestions.map((suggestion, i) => {
-                                    return (
-                                        <div key={i} {...getSuggestionItemProps(suggestion)}>
-                                            {suggestion.description}
-                                        </div>
-                                    );
-                                })}
+        <Wrapper apiKey={process.env.REACT_APP_MAPS_API_KEY} libraries={["places"]}>
+            <PlacesAutocomplete
+                value={address}
+                onChange={handleChange}
+                onSelect={handleSelect}
+            >
+                {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                    <div className='search-bar-wrapper'>
+                        <div className='search-bar-content'>
+                            <input className='search-bar-input' {...getInputProps({ placeholder: "Search destinations" })} />
+                            <div className='search-icon'>
+                                <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" height="12px" width="12px" display="block" stroke="currentcolor" strokeWidth="5.33333" overflow="visible"><g fill="none"><path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path></g></svg>
                             </div>
                         </div>
-                    )}
-                </PlacesAutocomplete>
-            </Wrapper>
-        </>
+                        <div>
+                            {loading && <div>Loading...</div>}
+                            {suggestions.map((suggestion, i) => {
+                                return (
+                                    <div key={i} {...getSuggestionItemProps(suggestion)}>
+                                        {suggestion.description}
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                )}
+            </PlacesAutocomplete>
+        </Wrapper>
     );
 };
 
