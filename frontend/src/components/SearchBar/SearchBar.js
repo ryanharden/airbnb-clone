bee18714d9b5cafae2f0b9b830925e7770372be6
Ryan Harden <ryanharden20@gmail.com>
2023-04-10 19:31:09 -0700

    search-suggestions css

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -79,11 +79,11 @@ const SearchBar = () => {
                                 <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" height="12px" width="12px" display="block" stroke="currentcolor" strokeWidth="5.33333" overflow="visible"><g fill="none"><path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path></g></svg>
                             </div>
                         </div>
-                        <div>
+                        <div className='search-bar-suggestions'>
                             {loading && <div>Loading...</div>}
                             {suggestions.map((suggestion, i) => {
                                 return (
-                                    <div key={i} {...getSuggestionItemProps(suggestion)}>
+                                    <div className="search-suggestion" key={i} {...getSuggestionItemProps(suggestion)}>
                                         {suggestion.description}
                                     </div>
                                 );
